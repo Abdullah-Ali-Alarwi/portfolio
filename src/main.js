8828ef9525ad622262118e708e6e65f7d872a3f2
Abdullah-Ali-Alariw <alarwi.pro@gmail.com>
2025-03-25 20:37:19 +0300

    Added Google Analytics tag

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './assets/main.css'
 import i18n from './i18n'
 import { createApp } from 'vue'
+import VueGtag from 'vue-gtag'
 import App from './App.vue'
 
-createApp(App).use(i18n).mount('#app')
+const app = createApp(App)
+
+// إضافة Google Analytics
+app.use(i18n)
+app.use(VueGtag, {
+  config: { id: 'G-MS7DKT38XH' } // ضع هنا Google Tag ID الخاص بك
+})
+
+app.mount('#app')
